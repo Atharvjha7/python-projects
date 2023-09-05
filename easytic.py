d4b28179a044349dc24b7ba4dd78e239cacedc4c
Atharvjha7 <115515862+Atharvjha7@users.noreply.github.com>
2023-09-04 21:58:52 -0700

    Update and rename randomtictac.py to easytic.py

--- easytic.py
+++ easytic.py
@@ -1,7 +1,7 @@
 import random
 '''This is the easy level computer of the game I made in finalticgame.py using only random choice of the legal spaces to move in to play
 Includes basic functionalities such as illegal move handling and other illegal input detection.
-After this, I tested the performance of the computer at randomtest.py All three computer players I made use no data but rather simple logic to ensure the best chance of winning for the given skillset'''
+After this, I tested the performance of the computer at easytest.py All three computer players I made use no data but rather simple logic to ensure the best chance of winning for the given skillset'''
 # Initialize the Tic Tac Toe board
 board = [' '] * 9
 player_marker = 'X'
