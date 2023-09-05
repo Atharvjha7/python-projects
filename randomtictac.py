f5ac1c2ce1bec1a95342b2ab2aa798c1f1b12e19
Atharvjha7 <115515862+Atharvjha7@users.noreply.github.com>
2023-09-04 21:22:58 -0700

    Update randomtictac.py

--- randomtictac.py
+++ randomtictac.py
@@ -1,5 +1,6 @@
 import random
-
+'''This is a simple tic tac toe AI without training I made for fun. The inital version, that is this version, just randomly selects off of the valid spaces on the grid. 
+Includes basic functionalities such as illegal move handling and other illegal input detection. After this, I tested the performance of the computer at randomtest.py'''
 # Initialize the Tic Tac Toe board
 board = [' '] * 9
 player_marker = 'X'
