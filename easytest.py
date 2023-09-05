42e66a7e39e69e8f58e4c605f55bd63209b972cc
Atharvjha7 <115515862+Atharvjha7@users.noreply.github.com>
2023-09-04 21:57:06 -0700

    Update and rename randomtest.py to easytest.py

--- easytest.py
+++ easytest.py
@@ -1,6 +1,6 @@
 import random
-''' This is a test of the random computer tic tac toe player I made in randomtictac.py, I am using 1000 random simulated games and outputting the results after. I made a non-random, improved version of this
-over in simpletictac.py .'''
+''' This is a test of the random computer tic tac toe player I made in easytic.py, I am using 1000 random simulated games and outputting the results after. I made a non-random, improved version of this
+over in mediumtic.py .'''
 # Function to print the Tic Tac Toe board
 def print_board(board):
     print(f"{board[0]} | {board[1]} | {board[2]}")
